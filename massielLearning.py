1b9b8db3661fc4d1f41367b368e8559a3223ca88
José María Mateos <chema@rinzewind.org>
2014-04-20 22:27:53 +0200

    Use ratio

--- massielLearning.py
+++ massielLearning.py
@@ -1,7 +1,7 @@
 import sys
 from massielRatio import massielRatio
+
 ratio = massielRatio(sys.stdin)
-ratio = float(ratio[1])/float(ratio[0])
 
 if ratio > 0.54 and ratio < 0.782:
     print 'Probably Massiel'
