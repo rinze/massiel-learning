980b449c445d0f87e5dd41f4aecb6d3b4cf33cb6
José María Mateos <chema@rinzewind.org>
2014-04-20 19:30:03 +0200

    Better punctuation removal

--- massielRatio.py
+++ massielRatio.py
@@ -1,14 +1,18 @@
+import string
 def massielRatio(dataInput):
     words = 0 # All the words in the document
     la = 0 # 'la'
 
+    punct = set(string.punctuation)
     for line in dataInput:
         if not line.startswith('#'):
             tk = line.lower().split()
-            tk = [filter(str.isalpha, x) for x in tk]
+            tk = [filter(lambda x : ''.join(ch for ch in x if ch not in punct),\
+                         x) for x in tk]
             words += len(tk)
             la += sum(map(lambda x : x == "la", tk))
             # 'la' is sometimes also codified as 'lalala'
             la += 3 * sum(map(lambda x : x == "lalala", tk))
 
+    print [words, la]
     return [words, la]
